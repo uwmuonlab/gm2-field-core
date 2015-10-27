f0e56c3ef30e39da970f01f7e49fe38a5b20a7ed
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-26 20:40:43 -0500

    Added shim analyzer with multipole fitting.

---
+++ include/shim_analyzer.hh
@@ -0,0 +1,52 @@
+#ifndef GM2_FIELD_CORE_INCLUDE_SHIM_ANALYZER_HH_
+#define GM2_FIELD_CORE_INCLUDE_SHIM_ANALYZER_HH_
+
+/*===========================================================================*\
+
+author: Matthias W. Smith
+email:  mwsmith2@uw.edu
+file:   shim_analyzer.hh
+
+about:  A class meant to handle the initial analysis of shim datasets.
+
+\*===========================================================================*/
+
+//--- std includes  ---------------------------------------------------------//
+
+//--- other includes --------------------------------------------------------//
+#include "TFile.h"
+#include "TTree.h"        
+#include "root/math/SMatrix.h"
+#include "root/math/SVector.h"
+
+//--- projects includes -----------------------------------------------------//
+#include "shim_structs.hh"
+#include "shim_dataset.hh"
+
+namespace gm2 {
+
+std::vector<double> fit_multipoles(platform_t platform);
+
+// class ShimAnalyzer {
+
+//  public:
+
+//   ShimAnalyzer(std::string rootfile) {
+//     data_ = ShimDataset(rootfile);
+//   };
+
+//   ShimAnalyzer(const ShimDatset &data) {
+//     data_ = data;
+//   }
+  
+//  private:
+
+//   ShimDataset data_;
+//   PlatformCoords platform_coords_;
+
+//   void FitMultipoles();
+// };
+
+} // ::gm2
+
+#endif
---
+++ src/shim_analyzer.cxx
@@ -0,0 +1,100 @@
+#include "shim_analyzer.hh"
+
+namespace gm2 {
+
+std::vector<double> fit_multipoles(platform_t platform)
+{
+  using namespace ROOT::Math;
+  // Solve a linear multipole fit of form y = Mx + eps.
+  // The minimizing solution is x - (M^T * M)-1*M^T = 0.
+
+  // Initialization.
+  const int nprobes = platform_nprobes;
+  static bool initialized = false;
+
+  static SMatrix<double, nprobes, num_multipoles> M;
+  static SMatrix<double, num_multipoles, nprobes> S;
+  static SVector<double, nprobes> y;
+  static SVector<double, num_multipoles> x;
+  static std::vector<double> x_ret(num_multipoles, 0.0);
+
+  if (!initialized) {
+
+    int n;
+    bool skew;
+    double rho, phi, val;
+
+    for (int i = 0; i < nprobes; ++i) {
+
+      for (int j = 0; j < num_multipoles; ++j) {
+
+        n = (j + 1) / 2;
+        skew = ((j % 2) == 0);
+
+        rho = platform_polar[i][0] / platform_rad;
+        phi = platform_polar[i][1];
+
+        if (j == 0) {
+          skew = false;
+        }
+
+        if (skew) {
+
+          val = sin(n * phi);
+
+          if (std::abs(val) < 1.0e-20) {
+            val = 0.0;
+          }          
+
+        } else {
+
+          val = cos(n * phi);
+
+          if (std::abs(val) < 1.0e-20) {
+            val = 0.0;
+          }
+        }
+
+        val *= pow(rho, n);
+        M(i, j) = val;
+      }
+    }
+
+    int rc;
+    double det;
+
+    SMatrix<double, num_multipoles, nprobes> M_t;
+    SMatrix<double, num_multipoles, num_multipoles> I;    
+    M_t = Transpose(M);
+    I = M_t * M;
+
+    I.Det2(det);
+    S = I.Inverse(rc) * M_t;
+
+    initialized = true;
+  }
+
+  for (int i = 0; i < nprobes; ++i) {
+    y[i] = platform.freq[i];
+  }
+
+  x = S * y;
+
+  for (int i = 0; i < num_multipoles; ++i) {
+    int n = (i + 1) / 2;
+    x_ret[i] = x[i];
+    // std::cout << i << ": ";
+    // for (int j = 0; j < nprobes; ++j) {
+    //   printf("% 1.1e,", M(j, i));
+    // }
+    // std::cout << std::endl;
+  }
+
+//  S.Print(std::cout);
+//  M.Print(std::cout);
+//  std::cout << M * x << std::endl;
+
+  return x_ret;
+}
+
+} // ::gm2
